734f43e85a993c6b8bbab7a484dcb35b363c6dd7
FTCHD <far@fetched>
2024-03-28 10:30:26 +0200

    login with Farcaster

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,72 @@
+'use client'
+import {
+    AuthKitProvider,
+    SignInButton,
+    type StatusAPIResponse,
+    useProfile,
+} from '@farcaster/auth-kit'
+import '@farcaster/auth-kit/styles.css'
+import { IconButton } from '@mui/joy'
+import { Stack, Typography } from '@mui/joy'
+import { getCsrfToken, signIn, signOut, useSession } from 'next-auth/react'
+import { useCallback } from 'react'
+import { LogOut } from 'react-feather'
+
+export const runtime = 'edge'
 
 export default function Home() {
-  return (
-    <main>
-      <div>
-      </div>
-    </main>
-  );
+    const sesh = useSession()
+
+    const { isAuthenticated } = useProfile()
+
+    const getNonce = useCallback(async () => {
+        const nonce = await getCsrfToken()
+        if (!nonce) throw new Error('Unable to generate nonce')
+        return nonce
+    }, [])
+
+    const handleLogin = useCallback((res: StatusAPIResponse) => {
+        signIn('credentials', {
+            message: res.message,
+            signature: res.signature,
+            name: res.username,
+            pfp: res.pfpUrl,
+            redirect: false,
+        })
+    }, [])
+
+    return (
+        <Stack height={'100%'} width={'100%'}>
+            <Stack
+                gap={2}
+                width={'100%'}
+                height={'100%'}
+                justifyContent={'center'}
+                alignItems={'center'}
+                textAlign={'center'}
+            >
+                <Typography level="h1" fontSize={'3rem'}>
+                    Welcome to Frametrain!
+                </Typography>
+                <Typography level="body-lg">Sign in with Farcaster to get started.</Typography>
+                <AuthKitProvider>
+                    {isAuthenticated || sesh?.status === 'authenticated' ? (
+                        <IconButton
+                            onClick={() => {
+                                signOut()
+                            }}
+                        >
+                            <LogOut size={16} />
+                        </IconButton>
+                    ) : (
+                        <SignInButton
+                            nonce={getNonce}
+                            onSuccess={handleLogin}
+                            onSignOut={() => signOut()}
+                        />
+                    )}
+                </AuthKitProvider>
+            </Stack>
+        </Stack>
+    )
 }
---
+++ auth.ts
@@ -0,0 +1,111 @@
+import { createAppClient, viemConnector } from '@farcaster/auth-client'
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const {
+    handlers: { GET, POST },
+    auth,
+} = NextAuth({
+    theme: {
+        logo: 'https://next-auth.js.org/img/logo/logo-sm.png',
+    },
+    providers: [
+        CredentialsProvider({
+            name: 'Sign in with Farcaster',
+            credentials: {
+                message: {
+                    label: 'Message',
+                    type: 'text',
+                    placeholder: '0x0',
+                },
+                signature: {
+                    label: 'Signature',
+                    type: 'text',
+                    placeholder: '0x0',
+                },
+                // In a production app with a server, these should be fetched from
+                // your Farcaster data indexer rather than have them accepted as part
+                // of credentials.
+                // ...jfc i love dEcEnTrAlIzaTiOn
+                name: {
+                    label: 'Name',
+                    type: 'text',
+                    placeholder: '0x0',
+                },
+                pfp: {
+                    label: 'Pfp',
+                    type: 'text',
+                    placeholder: '0x0',
+                },
+            },
+            authorize: async (credentials, req) => {
+                const { csrfToken } = (await req.json()) as any
+
+                const appClient = createAppClient({
+                    ethereum: viemConnector(),
+                })
+
+                const { success, fid } = await appClient.verifySignInMessage({
+                    message: credentials?.message as string,
+                    signature: credentials?.signature as `0x${string}`,
+                    domain: process.env.NEXT_PUBLIC_DOMAIN!,
+                    nonce: csrfToken,
+                })
+
+                if (!success) {
+                    return null
+                }
+
+                console.log('fid', fid)
+
+                return {
+                    id: fid.toString(),
+                    name: credentials.name as string,
+                    image: credentials?.pfp as string,
+                }
+            },
+        }),
+    ],
+    callbacks: {
+        jwt: async ({ token, user, account, profile, trigger }) => {
+            if (user) token.user = user
+            if (user) {
+                token.uid = user.id
+            }
+            return token
+        },
+        session: async ({ session, token, user }) => {
+            if (token.user) session.user = { ...session.user, id: (token.user as any).id }
+            //  session.user.uid = user.uid;
+            return session
+        },
+    },
+    events: {
+        async signIn(message) {
+            /* on successful sign in */
+        },
+        async signOut(message) {
+            /* on signout */
+        },
+        async createUser(message) {
+            /* user created */
+        },
+        async updateUser(message) {
+            /* user updated - e.g. their email was verified */
+        },
+        async linkAccount(message) {
+            /* account (e.g. Twitter) linked to a user */
+        },
+        async session(message) {
+            /* session is active */
+        },
+    },
+    pages: {
+        signIn: '/login',
+        signOut: '/',
+        error: '/', // Error code passed in query string as ?error=
+        verifyRequest: '/', // (used for check email message)
+        // newUser: null // If set, new users will be directed here on first sign in
+    },
+    debug: true,
+})
