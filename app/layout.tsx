fe23050a47d415db949d27780fce5c7d2d2be208
FTCHD <far@fetched>
2024-03-28 10:31:36 +0200

    add missing SessionProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,32 @@
 import ThemeRegistry from '@/components/foundation/ThemeRegistry'
-import type React from "react";
-import { Toaster } from "react-hot-toast";
+import { SessionProvider } from 'next-auth/react'
+import type React from 'react'
+import { Toaster } from 'react-hot-toast'
 
 export default function Layout(props: { children: React.ReactNode }) {
-	return (
-		<ThemeRegistry>
-			<html lang="en">
-				<body
-					style={{
-						height: "100dvh",
-						width: "100dvw",
-						margin: 0,
-						padding: 0,
-					}}
-				>
-					{props.children}
+    return (
+        <SessionProvider>
+            <ThemeRegistry>
+                <html lang="en">
+                    <body
+                        style={{
+                            height: '100dvh',
+                            width: '100dvw',
+                            margin: 0,
+                            padding: 0,
+                        }}
+                    >
+                        {props.children}
 
-					<Toaster
-						position="bottom-center"
-						toastOptions={{
-							duration: 3000,
-						}}
-					/>
-				</body>
-			</html>
-		</ThemeRegistry>
-	);
+                        <Toaster
+                            position="bottom-center"
+                            toastOptions={{
+                                duration: 3000,
+                            }}
+                        />
+                    </body>
+                </html>
+            </ThemeRegistry>
+        </SessionProvider>
+    )
 }
