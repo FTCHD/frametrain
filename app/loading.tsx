c03efbf425788760dd4cc27a782379fa973d276d
FTCHD <far@fetched>
2024-03-28 11:14:04 +0200

    add FUNNY_MESSAGES to loading state

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,22 @@
 
-import { Box, LinearProgress } from '@mui/joy'
+import { Box, LinearProgress, Stack, Typography } from '@mui/joy'
+
+const FUNNY_MESSAGES = [
+    "It's not always #000000 and #FFFFFF",
+    'Deploying killer robots...',
+    'Making the magic happen...',
+    'Doing the thing...',
+    'Calling the shots...',
+    'Developers, developers, developers.',
+    'Time is precious, waste it wisely!',
+    'Low battery. Please charge.',
+    'Waiting in line..',
+    'Procedurally doing nothing...',
+    'DO hold your breath ;)',
+    'Breeding more bits...',
+    'Changing brake fluid...',
+    'Sending IP to FBI...',
+]
 
 export default function Loading() {
     return (
@@ -14,12 +31,23 @@ export default function Loading() {
                 width: '100%',
             }}
         >
-            <LinearProgress
-                size="lg"
-                thickness={30}
-                color="primary"
-                sx={{ width: '100%', maxWidth: '500px' }}
-            />
+            <Stack
+                textAlign={'center'}
+                spacing={4}
+                width={'100%'}
+                justifyContent={'center'}
+                alignItems={'center'}
+            >
+                <LinearProgress
+                    size="lg"
+                    thickness={30}
+                    color="primary"
+                    sx={{ width: '100%', maxWidth: '500px' }}
+                />
+                <Typography level="h4" color="primary">
+                    {FUNNY_MESSAGES[Math.floor(Math.random() * FUNNY_MESSAGES.length)]}
+                </Typography>
+            </Stack>
         </Box>
     )
 }
