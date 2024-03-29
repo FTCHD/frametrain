86c421a4d4b197421256cb7a5807d7e64ff9e712
FTCHD <far@fetched>
2024-03-29 11:21:30 +0200

    layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,19 @@
 import ThemeRegistry from '@/components/foundation/ThemeRegistry'
+import type { Metadata } from 'next'
 import { SessionProvider } from 'next-auth/react'
 import type React from 'react'
 import { Toaster } from 'react-hot-toast'
 import './global.css'
+
+export const metadata: Metadata = {
+    title: {
+        default: 'FrameTrain',
+        template: '%s | FrameTrain',
+    },
+    icons: {
+        icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚂</text></svg>',
+    },
+}
 
 export default function Layout(props: { children: React.ReactNode }) {
     return (
