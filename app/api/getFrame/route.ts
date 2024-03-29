286923bbf9486888d1876559a0b51697aef14cbe
FTCHD <far@fetched>
2024-03-29 11:19:30 +0200

    debug routes

---
+++ app/api/getFrame/route.ts
@@ -0,0 +1,16 @@
+
+export const dynamic = 'force-dynamic';
+
+export async function POST(req: Request) {
+  const data = await req.json() as any
+  try {
+    const response = await fetch(data.url);
+    const text = await response.text();
+    return Response.json({ html: text }, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching frame:', error);
+    return Response.json({}, { status: 500, statusText: 'Internal Server Error' });
+  }
+}
+
+
---
+++ app/api/postFrame/route.ts
@@ -0,0 +1,31 @@
+import { getMockFrameRequest } from '@coinbase/onchainkit/frame'
+
+export async function POST(req: Request) {
+    const data = (await req.json()) as any
+
+    console.log('POSTED', JSON.stringify(data))
+
+    const { frameData, options } = data
+    const postUrl = frameData.url
+    const debugPayload = getMockFrameRequest(
+        { untrustedData: frameData, trustedData: { messageBytes: '' } },
+        options
+    )
+
+    const res = await fetch(postUrl, {
+        method: 'POST',
+        body: JSON.stringify(debugPayload),
+        redirect: 'manual',
+    })
+
+    // if (res.status === 302) {
+    //     const redirectUrl = res.headers.get('Location')
+    //     return Response.json({ redirectUrl })
+    // }
+
+    const html = await res.text()
+
+    console.log('HTML', JSON.stringify(html).substring(0, 20))
+
+    return Response.json({ html })
+}
