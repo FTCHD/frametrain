fb608c642776c53933de23039b54e426f75f6f1e
FTCHD <far@fetched>
2024-03-30 01:33:01 +0200

    get all styles in `loadGoogleFontAllVariants`

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -34,7 +34,7 @@ export async function loadGoogleFontAllVariants(
     const googleFontUrl = `https://fonts.googleapis.com/css2?family=${fontName.replace(
         ' ',
         '+'
-    )}:wght@100;200;300;400;500;600;700;800;900&display=swap`
+    )}:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap`
 
     const response = await fetch(googleFontUrl)
     const cssText = await response.text()
