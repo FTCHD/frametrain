a93efd518881a199579f59a45c1f7d78a73643ca
FTCHD <far@fetched>
2024-03-29 11:34:49 +0200

    disable warnings

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,18 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    poweredByHeader: false,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        ignoreBuildErrors: true,
+    },
+}
 
 export default nextConfig;
