6f02c61a5ef63fa966f1d6da10c1ab88ab5fe640
kirtirajsinh <kirtiraj3738@gmail.com>
2024-04-05 12:36:08 +0530

    Refactor ProjectCard component with Shadcn components

--- components/home/ProjectCard.tsx
+++ components/home/ProjectCard.tsx
@@ -1,54 +1,35 @@
 'use client'
-import { AspectRatio, Card, CardContent, Chip, Link, Typography } from '@mui/joy'
+// import { AspectRatio, Card, CardContent, Chip, Link, Typography } from '@mui/joy'
+// import { Card } from 'card';
 import NextImage from 'next/image'
-import NextLink from 'next/link'
-
+import Link from 'next/link'
+import { Card, CardContent, CardHeader } from '../ui/card'
 export default function ProjectCard({
     frame,
 }: { frame: { id: string; name: string; preview: string; currentMonthCalls: number } }) {
     const { id, name, preview, currentMonthCalls } = frame
     return (
         <Card
+            className="flex flex-row items-center   hover:border-ring hover:drop-shadow-md w-64"
             key={id}
-            variant="outlined"
-            orientation="horizontal"
-            sx={{
-                width: 320,
-                '&:hover': {
-                    boxShadow: 'md',
-                    borderColor: 'neutral.outlinedHoverBorder',
-                },
-            }}
         >
-            <AspectRatio ratio="1" sx={{ width: 90 }}>
+            <CardHeader className="p-2 w-24">
                 <NextImage
                     src={`data:image/svg+xml;base64,${preview}`}
                     alt={name}
-                    fill={true}
+                    width={320}
+                    height={180}
                     objectFit="cover"
                 />
-            </AspectRatio>
-            <CardContent
-                sx={{
-                    gap: 2,
-                }}
-            >
-                <Link
-                    overlay={true}
-                    href={`/frame/${id}`}
-                    underline="none"
-                    sx={{ color: 'text.tertiary', textDecoration: 'none' }}
-                    component={NextLink}
-                >
-                    <Typography level="title-lg" id="card-description">
-                        {name}
-                    </Typography>
-                </Link>
-
-                <Chip variant="outlined" color="primary" size="sm" sx={{ pointerEvents: 'none' }}>
-                    {currentMonthCalls === 0 ? 'Not used yet' : `${currentMonthCalls} calls`}
-                </Chip>
-            </CardContent>
+            </CardHeader>
+            <Link href={`/frame/${id}`}>
+                <CardContent className="space-y-4 items-center">
+                    <h1 className="font-medium">{name}</h1>
+                    <div className=" bg-border font-medium text-xs px-1 rounded-full border border-border">
+                        {currentMonthCalls === 0 ? 'Not used yet' : `${currentMonthCalls} calls`}
+                    </div>
+                </CardContent>
+            </Link>
         </Card>
     )
 }
