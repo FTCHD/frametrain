6a10c5681f58d6737d88ffcc23c8e2fa29744296
kirtirajsinh <kirtiraj3738@gmail.com>
2024-04-13 19:32:52 +0530

    Refactor Inspector component to use ShadcnUI components and Tailwind CSS

--- templates/basic/Inspector.tsx
+++ templates/basic/Inspector.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, Stack } from '@mui/joy'
+import { Button } from '@/components/ui/button'
 import type { Config } from '.'
 
 
@@ -8,9 +8,9 @@ export default function Inspector({
     update,
 }: { config: Config; update: (props: any) => void }) {
     return (
-        <Stack width={'100%'} height={'100%'} gap={5}>
-            Add Images
+        <div className="w-full h-full space-y-4">
+            <h2 className="text-base">Add Images</h2>
             <Button onClick={() => update({ vote: { buttons: [] } })}>Delete All</Button>
-        </Stack>
+        </div>
     )
 }
