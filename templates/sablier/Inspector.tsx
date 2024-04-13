a3bb1fdbd4078d414117ee5b3ea4a7c1cbd28ac0
kirtirajsinh <kirtiraj3738@gmail.com>
2024-04-13 19:33:54 +0530

    refactor Sablier Inspector to use ShadcnUI components

--- templates/sablier/Inspector.tsx
+++ templates/sablier/Inspector.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FormHelperText, Input, Stack, Typography } from '@mui/joy'
+import { Input } from '@/components/ui/input'
 import { useEffect, useRef } from 'react'
 import type { Config } from '.'
 import { getStreamData, getStreamType } from './utils/actions'
@@ -22,6 +22,7 @@ export default function Inspector({
     }, [config.streamId])
 
     async function handleInputChange(e: any) {
+		
         const streamId = e.target.value
 
         const data = await getStreamData(streamId)
@@ -32,18 +33,14 @@ export default function Inspector({
     }
 
     return (
-        <Stack width={'100%'} height={'100%'} gap={5}>
-            <Stack direction={'column'} gap={2}>
-                <Typography level="title-lg">Stream ID</Typography>
-                <Input
-                    size="lg"
-                    slotProps={{ input: { ref: inputRef } }}
-                    onChange={handleInputChange}
-                />
-                <FormHelperText>
+        <div className="h-full w-full space-y-4">
+            <div className="flex flex-col space-y-2">
+                <h2 className="text-lg font-bold">Stream ID</h2>
+                <Input ref={inputRef} onChange={handleInputChange} />
+                <p className="text-sm text-muted-foreground">
                     In "XX-YY-ZZ" format, eg. <code>LL-1-121</code>.
-                </FormHelperText>
-            </Stack>
-        </Stack>
+                </p>
+            </div>
+        </div>
     )
 }
