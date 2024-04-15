549f66d543877b1a4e5867898e53c5186681d887
FTCHD <far@fetched>
2024-04-15 17:15:37 +0300

    enable tweet template

--- templates/index.ts
+++ templates/index.ts
@@ -2,10 +2,12 @@
 import pdf from './pdf'
 import poll from './poll'
 import sablier from './sablier'
+import twitter from './twitter'
 
 export default {
     // basic,
     pdf,
     poll,
     sablier,
+    twitter,
 }
