34121fc92863552915843e5a08d8e3a8c4c5bcc9
FTCHD <far@fetched>
2024-04-15 16:59:39 +0300

    Scrape API v1

--- lib/scrape.ts
+++ lib/scrape.ts
@@ -1,78 +1,55 @@
 'use server'
-import { Readability } from '@mozilla/readability'
-import { unstable_noStore as noStore } from 'next/cache'
-import { NodeHtmlMarkdown } from 'node-html-markdown'
-import { Parser } from 'htmlparser2';
 
-function htmlToMarkdown(html: string, url: string) {
-    noStore()
+async function twitterUser(username: string) {
+    const res = await fetch(`${process.env.SCRAPER_URL}/twitter/profile/${username}`, {
+        cache: 'no-store',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res) => res.json())
+        .catch(console.error)
 
-	const parser = new Parser({
-		onopentag(name, attributes) {
-		  // Handle opening tags if needed
-		},
-		ontext(text) {
-		  // Handle text content if needed
-		},
-		onclosetag(tagname) {
-		  // Handle closing tags if needed
-		},
-	  }, {
-		decodeEntities: true,
-	  });
-	  
-	  parser.write(html);
-	  parser.end();
-	  
-	  
-	const doc = parser.dom
-	
-    const article = new Readability(doc, url).parse()
-    // return article!
-	
-	console.log('PARSED', doc)
-
-    const content = NodeHtmlMarkdown.translate(article?.content || '', {})
-
-    return { ...article, content }
+    return res
 }
 
-async function renderWebpage(url: string) {
-    noStore()
+async function twitterTweet(id: string) {
+    const res = await fetch(`${process.env.SCRAPER_URL}/twitter/tweet/${id}`, {
+        cache: 'no-store',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res) => res.json())
+        .catch(console.error)
 
-    const input = {
-        gotoOptions: { waitUntil: 'networkidle2' },
-        url,
-    }
-
-    const res = await fetch(
-        `${process.env.BROWSERLESS_URL}/chrome/content?token=${process.env.BROWSERLESS_TOKEN}`,
-        {
-            cache: 'no-store',
-            body: JSON.stringify(input),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-        }
-    )
-    const html = await res.text()
-
-    return html
+    return res
 }
 
-export async function parseWebpage(url: string) {
-    noStore()
+async function scrape({
+    url,
+    readability,
+    markdown,
+}: { url: string; readability: boolean; markdown: boolean }) {
+    const path = markdown ? 'markdown' : readability ? 'readability' : 'scrape'
 
-    try {
-        const html = await renderWebpage(url)
+    const res = await fetch(`${process.env.SCRAPER_URL}/${path}/${encodeURI(url)}`, {
+        cache: 'no-store',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res) => res.json())
+        .catch(console.error)
 
-        const article = htmlToMarkdown(html, url)
-
-        return { content: article.content, title: article?.title, url, website: article?.siteName }
-    } catch (error) {
-        console.error(error)
-        return { content: '抓取失败', errorMessage: (error as any).message, url }
-    }
+    return res
 }
 
+
+export default {
+    twitter: {
+        profile: twitterUser,
+        tweet: twitterTweet,
+    },
+    scrape,
+}
