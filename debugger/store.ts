f29498e299e3af1512c312847da65825bced6b72
FTCHD <far@fetched>
2024-04-29 02:56:28 +0300

    [app] clear old debugger

--- debugger/store.ts
+++
@@ -1,9 +0,0 @@
-import type { MockFrameRequestOptions } from '@coinbase/onchainkit/frame'
-import { atom } from 'jotai'
-import type { fetchFrame } from './getFrame'
-
-// We store an array here so that we can step through history, e.g. seeing the
-// chain of responses through a number of frame actions.
-export const frameResultsAtom = atom<Awaited<ReturnType<typeof fetchFrame>>[]>([])
-
-export const mockFrameOptionsAtom = atom<MockFrameRequestOptions>({})
