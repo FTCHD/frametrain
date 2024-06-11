d8c69ef1e659dcf7e2a4b41e2928639cc0d48f8b
Qedric <qedricjames@gmail.com>
2024-06-11 20:24:49 +0200

    first commit

---
+++ templates/medium/utils.ts
@@ -0,0 +1,167 @@
+import type { PDFDocumentProxy, PDFPageProxy } from 'pdfjs-dist'
+import { scrape } from '@/sdk/scrape'
+
+interface MediumMetadata {
+    authorUrl: string | null;
+    author: string | null;
+    title: string | null;
+    image: string | null;
+}
+
+// One tag and its contents
+export type Element = {
+    tag: string
+    text: string
+    nestedTags?: {position:number, tag:Element}[]
+}
+
+// A page will be made up of an array of elements
+export type Page = Element[]
+
+export type Article = {
+    pages: Page[]
+    url: string
+    metadata: MediumMetadata
+}
+
+export async function getMediumArticle(url: string): Promise<Article> {
+    
+    console.log('scraping medium article', url)
+
+    const res = await scrape({ url })
+
+    const extractedText = extractTextFromMediumHTML(res.content)
+    const metadata = getMediumMetadata(res.content)
+
+    console.log(metadata)
+
+    const article = { pages: paginateElements(extractedText, 1200, metadata), url, metadata } // character limit
+
+    return article
+}
+
+// Attempts to pull out only the relevant content from the HTML
+function extractTextFromMediumHTML(html: string): Element[] {
+    // Create a new DOMParser
+    const parser = new DOMParser()
+
+    // Use the DOMParser to turn the HTML string into a Document
+    const doc = parser.parseFromString(html, 'text/html')
+
+    // Get the article element and its relevant child elements
+    const article = doc.body.querySelector('article')
+    const elements = article?.querySelectorAll('p, h1, h2, h3, h4, h5, h6')
+
+    if (!elements) return []
+
+    const elementsArray: HTMLElement[] = Array.from(elements) as HTMLElement[]
+
+    // Helper function to extract nested tags with character positions
+    function extractNestedTags(element: HTMLElement): { position: number, tag: Element }[] {
+        const nestedTags: { position: number, tag: Element }[] = []
+        let charIndex = 0
+        
+        element.childNodes.forEach((node) => {
+            if (node.nodeType === Node.TEXT_NODE) {
+                charIndex += node.textContent?.length || 0
+            } else if (node.nodeType === Node.ELEMENT_NODE) {
+                const childElement = node as HTMLElement
+                if (childElement.tagName === 'STRONG' || childElement.tagName === 'EM') {
+                    nestedTags.push({
+                        position: charIndex,
+                        tag: {
+                            tag: childElement.tagName,
+                            text: childElement.textContent || ''
+                        }
+                    })
+                    charIndex += childElement.textContent?.length || 0
+                }
+            }
+        })
+        return nestedTags
+    }
+
+    const cleanedElements: Element[] = elementsArray.map(e => {
+        const nestedTags = extractNestedTags(e)
+        return {
+            tag: e.tagName,
+            text: e.textContent || '',
+            nestedTags: nestedTags.length > 0 ? nestedTags : undefined
+        }
+    })
+
+    return cleanedElements
+}
+
+
+function paginateElements(elements: Array<{ tag: string; text: string }>, charLimit: number, metadata:MediumMetadata): Page[] {
+    let pages: Page[] = []
+    let currentPage: Page = []
+    let currentCharCount = 0
+
+    // ignore elements if their text is equal to certain keywords including the author, 'Follow', 'Listen', 'Share', or just a number
+    elements = elements.filter(element => {
+        const text = element.text.trim()
+        return !['Follow', 'Listen', 'Share', metadata.author, metadata.title, 'About', 'Contact', 'Subscribe', 'Top highlight'
+        ].includes(text) && !text.match(/^\d+(\.\d+)?[KMB]?$/)
+    })
+
+    elements.forEach(element => {
+        const elementTextLength = element.text?.length ?? 0
+    
+        if (currentCharCount + elementTextLength <= charLimit) {
+            currentPage.push(element)
+            currentCharCount += elementTextLength
+        } else {
+            pages.push(currentPage)
+            currentPage = [element]
+            currentCharCount = elementTextLength
+        }
+    })
+
+    if (currentPage.length > 0) {
+        pages.push(currentPage)
+    }
+
+    return pages
+}
+
+// Function to extract metadata from the provided HTML content
+function getMediumMetadata(htmlContent: string): MediumMetadata {
+    const tempDiv = document.createElement('div')
+    tempDiv.innerHTML = htmlContent
+
+    // Initialize metadata object
+    const metadata: MediumMetadata = {
+        authorUrl: null,
+        author: null,
+        title: null,
+        image: null,
+    }
+
+    // Extract metadata from meta tags
+    const metaTags = tempDiv.querySelectorAll('meta[data-rh="true"]')
+    metaTags.forEach((tag) => {
+        const property = tag.getAttribute('property')
+        const name = tag.getAttribute('name')
+        const content = tag.getAttribute('content')
+
+        if (property === 'article:author') {
+            metadata.authorUrl = content
+        } else if (name === 'author') {
+            metadata.author = content
+        } else if (property === 'og:title') {
+            metadata.title = content
+        } else if (property === 'og:image') {
+            metadata.image = content
+        }
+    })
+
+    return metadata
+}
+
+export function createMediumPage(article: any, page: number) {
+    console.log(page, article)
+}
+
+export default getMediumArticle
---
+++ templates/medium/views/Page.tsx
@@ -0,0 +1,67 @@
+import { slice } from "viem";
+import { Page } from "../utils";
+
+export default function PageView({
+    page,
+    currentPage,
+    slideCount
+}: { page: Page, currentPage: number, slideCount: number}) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexFlow: 'column nowrap',
+                alignItems: 'stretch',
+                width: '100%',
+                height: '100%',
+                backgroundColor: '#fff',
+                color: '#000',
+            }}
+        >
+            <div style={{
+                display: 'flex',
+                flexFlow: 'column nowrap',
+                padding: '36px',
+                fontFamily: 'Georgia',
+                fontSize: '18px',
+             }}>
+                {page.map((element, index) => {
+                    switch (element.tag) {
+                        case 'P':
+                            return <p key={index} style={{ margin: '10px 0' }}>{element.text}</p>
+                        case 'H1':
+                            return <h1 key={index} style={{ fontSize: '30px', margin: '10px 0' }}>{element.text}</h1>
+                        case 'H2':
+                            return <h2 key={index} style={{ fontSize: '24px', margin: '10px 0' }}>{element.text}</h2>
+                        case 'H3':
+                            return <h3 key={index} style={{ margin: '10px 0' }}>{element.text}</h3>
+                        case 'H4':
+                            return <h4 key={index} style={{ margin: '10px 0' }}>{element.text}</h4>
+                        case 'H5':
+                            return <h5 key={index} style={{ margin: '10px 0' }}>{element.text}</h5>
+                        case 'H6':
+                            return <h6 key={index} style={{ margin: '10px 0' }}>{element.text}</h6>
+                        case 'STRONG':
+                            return <strong key={index} style={{ margin: '10px 0' }}>{element.text}</strong>
+                        case 'EM':
+                            return <em key={index} style={{ margin: '10px 0' }}>{element.text}</em>
+                        default:
+                            return <span key={index} style={{ margin: '10px 0' }}>{element.text}</span>
+                    }
+                })}
+            </div>
+            <div style={{
+                display: 'flex',
+                flexFlow: 'column nowrap',
+                padding: '36px',
+                fontFamily: 'Georgia',
+                fontSize: '11px',
+                position: 'absolute',
+                bottom: '12px',
+                left: '0'
+             }}>
+                { currentPage }/{ slideCount }
+             </div>
+        </div>
+    )
+}
