2af370baabe374236210616c7c9ec008591daa0d
TheVirginBrokey <itswaptik@gmail.com>
2024-06-11 20:38:58 +0000

    fix biome format

--- templates/index.ts
+++ templates/index.ts
@@ -1,17 +1,17 @@
-import discourse from "./discourse";
+import discourse from './discourse'
 // import basic from './basic'
-import pdf from "./pdf";
-import poll from "./poll";
-import quizzlet from "./quizzlet";
-import sablier from "./sablier";
-import twitter from "./twitter";
+import pdf from './pdf'
+import poll from './poll'
+import quizzlet from './quizzlet'
+import sablier from './sablier'
+import twitter from './twitter'
 
 export default {
-  // basic,
-  pdf,
-  poll,
-  sablier,
-  twitter,
-  discourse,
-  quizzlet,
-};
+    // basic,
+    pdf,
+    poll,
+    sablier,
+    twitter,
+    discourse,
+    quizzlet,
+}
