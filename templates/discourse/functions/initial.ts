ad1d3d539e858b07159d1933aae1082db184ec52
FTCHD <far@fetched>
2024-06-11 18:05:44 +0300

    [template] Discourse v1

--- templates/discourse/functions/initial.ts
+++ templates/discourse/functions/initial.ts
@@ -5,19 +5,35 @@ import type { Config, State } from '..'
 import CoverView from '../views/Cover'
 
 export default async function initial(config: Config, state: State): Promise<BuildFrameData> {
-    const urbanist = await loadGoogleFontAllVariants('Urbanist')
+    const font = await loadGoogleFontAllVariants(config.highlightFont || 'Urbanist')
+
+    const thread = await fetch(config.discourseJson)
+        .then((res) => res.json())
+        .catch(console.error)
 
     return {
         buttons: [
             {
-                label: 'Open Thread',
+                label: 'Read',
+            },
+            {
+                label: 'Latest Post',
+            },
+            {
+                label: 'View',
+                action: 'link',
+                target: config.discourseLink,
             },
         ],
         aspectRatio: '1.91:1',
-        fonts: urbanist,
+        fonts: font,
         component: CoverView({
             title: config.title,
-            postCount: config.postCount,
+            postCount: thread.posts_count,
+            viewCount: thread.views,
+            backgroundColor: config.backgroundColor,
+            highlightColor: config.highlightColor,
+            highlightFont: config.highlightFont,
         }),
         functionName: 'post',
     }
