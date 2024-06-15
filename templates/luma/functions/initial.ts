9c599f92ff3b983333ca3b4f1364a499c66734c3
TheVirginBrokey <itswaptik@gmail.com>
2024-06-15 10:38:14 +0000

    fix bug in rendering frame image

--- templates/luma/functions/initial.ts
+++ templates/luma/functions/initial.ts
@@ -1,11 +1,9 @@
 'use server'
 
 import type { BuildFrameData } from '@/lib/farcaster'
-import { loadGoogleFontAllVariants } from '@/sdk/fonts'
 import type { Config, State } from '..'
 
 export default async function initial(config: Config, _state: State): Promise<BuildFrameData> {
-    const roboto = await loadGoogleFontAllVariants('Roboto')
     const eventUrl = 'https://lu.ma/' + config.eventId
 
     return {
@@ -13,7 +11,6 @@ export default async function initial(config: Config, _state: State): Promise<Bu
             { label: 'Visit event page', target: eventUrl },
             { label: 'Create a lu.ma Preview Frame', target: 'https://frametra.in' },
         ],
-        fonts: roboto,
-        component: `${process.env.NEXT_PUBLIC_HOST}/api/og/luma/${config.eventId}`,
+        image: `${process.env.NEXT_PUBLIC_HOST}/api/og/luma/${config.eventId}`,
     }
 }
