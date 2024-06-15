1606a88e23c3598bfdb776ca4bea654db8cc81f9
TheVirginBrokey <itswaptik@gmail.com>
2024-06-15 08:38:45 +0000

    fix: use `NEXT_PUBLIC_HOST` instead of `VERCEL_URL`

--- templates/luma/functions/initial.ts
+++ templates/luma/functions/initial.ts
@@ -8,16 +8,12 @@ export default async function initial(config: Config, _state: State): Promise<Bu
     const roboto = await loadGoogleFontAllVariants('Roboto')
     const eventUrl = 'https://lu.ma/' + config.eventId
 
-    const isDev = process.env.NODE_ENV === 'development'
-
     return {
         buttons: [
             { label: 'Visit event page', target: eventUrl },
             { label: 'Create a lu.ma Preview Frame', target: 'https://frametra.in' },
         ],
         fonts: roboto,
-        component: `http${isDev ? '' : 's'}://${
-            isDev ? 'localhost:3000' : process.env.VERCEL_URL
-        }/api/og/luma/${config.eventId}`,
+        component: `${process.env.NEXT_PUBLIC_HOST}/api/og/luma/${config.eventId}`,
     }
 }
