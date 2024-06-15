559431c3db0f07da5b95a8dbfa70411359eda4a0
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2024-06-16 00:37:55 +0900

    update action tx

--- templates/github/functions/page.ts
+++ templates/github/functions/page.ts
@@ -2,6 +2,33 @@
 import type { BuildFrameData, FrameActionPayload } from '@/lib/farcaster'
 import type { Config, State } from '..'
 import PageView from '../views/Page'
+import { Abi, encodeFunctionData } from 'viem'
+
+const contractAbi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+]
 
 export default async function page(
     body: FrameActionPayload,
@@ -9,15 +36,32 @@ export default async function page(
     state: State,
     params: any
 ): Promise<BuildFrameData> {
-    const { tokenAddress, to, value } = config
+    const { tokenAddress, to } = config
     const inputToken = body.untrustedData.inputText
+
+    const calldata = encodeFunctionData({
+        abi: contractAbi as Abi,
+        functionName: "transfer",
+        args: [to, inputToken]
+    })
+
+    const txData = {
+        chainId: "eip155:84532",  // base sepolia
+        method: "eth_sendTransaction",
+        params: {
+            abi: contractAbi as Abi,
+            to: tokenAddress as `0x${string}`,
+            data: calldata as any,
+            value: "0" // only send erc20
+        },
+    }
 
     return {
         buttons: [
             {
                 label: 'Send',
-                action: 'post',
-                target: `${process.env.NEXT_PUBLIC_HOST}/api/tx?tokenAddress=${tokenAddress}&to=${to}&value=${inputToken}`
+                action: 'tx',
+                target: JSON.stringify(txData)
             },
             {
                 label: 'Back',
