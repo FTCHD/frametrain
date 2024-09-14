506ccbf2f0f044041e319310b8549780980b0abe
FTCHD <far@fetched>
2024-09-14 21:20:53 +0300

    [template] Meme hide sections

--- templates/meme/Inspector.tsx
+++ templates/meme/Inspector.tsx
@@ -64,83 +64,77 @@ export default function Inspector() {
                 </Select>
             </Configuration.Section>
 
-            <Configuration.Section title="Thumbnail" description="Preview your meme template">
-                {selectedMeme ? (
+            {selectedMeme && (
+                <Configuration.Section title="Thumbnail" description="Preview your meme template">
                     <div className="flex flex-row flex-wrap gap-4">
                         <img src={selectedMeme.url} width={200} height={200} alt="PDF Slide" />
                     </div>
-                ) : (
-                    <h3 className="text-lg font-semibold">No meme selected</h3>
-                )}
-            </Configuration.Section>
+                </Configuration.Section>
+            )}
 
-            <Configuration.Section title="Captions" description="Add your meme captions">
-                {selectedMeme ? (
-                    <>
-                        <div className="flex flex-col gap-2">
-                            {Array.from({ length: selectedMeme?.positions || 1 }).map((_, i) => (
-                                <Input
-                                    key={i}
-                                    defaultValue={captions[i]}
-                                    disabled={!selectedMeme || generating}
-                                    className="text-lg"
-                                    placeholder={`Caption ${i + 1}`}
-                                    onChange={(e) => {
-                                        const newCaptions = [...captions]
-                                        newCaptions[i] = e.target.value
-                                        setCaptions(newCaptions)
-                                    }}
-                                />
-                            ))}
-                        </div>
+            {selectedMeme && (
+                <Configuration.Section title="Captions" description="Add your meme captions">
+                    <div className="flex flex-col gap-2">
+                        {Array.from({ length: selectedMeme?.positions || 1 }).map((_, i) => (
+                            <Input
+                                key={i}
+                                defaultValue={captions[i]}
+                                disabled={!selectedMeme || generating}
+                                className="text-lg"
+                                placeholder={`Caption ${i + 1}`}
+                                onChange={(e) => {
+                                    const newCaptions = [...captions]
+                                    newCaptions[i] = e.target.value
+                                    setCaptions(newCaptions)
+                                }}
+                            />
+                        ))}
+                    </div>
 
-                        <Button
-                            disabled={!selectedMeme || generating}
-                            onClick={async () => {
-                                if (!selectedMeme) return
-                                if (captions.length !== selectedMeme.positions) return
+                    <Button
+                        disabled={!selectedMeme || generating}
+                        onClick={async () => {
+                            if (!selectedMeme) return
+                            if (captions.length !== selectedMeme.positions) return
 
-                                setGenerating(true)
+                            setGenerating(true)
 
-                                createMeme(captions, selectedMeme.id)
-                                    .then(async (memeUrl) => {
-                                        updateConfig({
-                                            memeUrl,
-                                            template: {
-                                                id: selectedMeme.id,
-                                                name: selectedMeme.name,
-                                                url: selectedMeme.url,
-                                                captions,
-                                            },
-                                        })
-                                        setSelectedMeme(undefined)
-                                        setCaptions([])
+                            createMeme(captions, selectedMeme.id)
+                                .then(async (memeUrl) => {
+                                    updateConfig({
+                                        memeUrl,
+                                        template: {
+                                            id: selectedMeme.id,
+                                            name: selectedMeme.name,
+                                            url: selectedMeme.url,
+                                            captions,
+                                        },
                                     })
-                                    .catch((err) => {
-                                        const error = err as Error
-                                        toast.remove()
-                                        toast.error(error.message)
-                                    })
-                                    .finally(() => {
-                                        setGenerating(false)
-                                    })
-                            }}
-                            size="lg"
-                            className="w-full bg-border hover:bg-secondary-border text-primary"
-                        >
-                            {generating ? <LoaderIcon className="animate-spin" /> : 'Create'}
-                        </Button>
-                    </>
-                ) : (
-                    <h3 className="text-lg font-semibold">No meme selected</h3>
-                )}
-            </Configuration.Section>
+                                    setSelectedMeme(undefined)
+                                    setCaptions([])
+                                })
+                                .catch((err) => {
+                                    const error = err as Error
+                                    toast.remove()
+                                    toast.error(error.message)
+                                })
+                                .finally(() => {
+                                    setGenerating(false)
+                                })
+                        }}
+                        size="lg"
+                        className="w-full bg-border hover:bg-secondary-border text-primary"
+                    >
+                        {generating ? <LoaderIcon className="animate-spin" /> : 'Create'}
+                    </Button>
+                </Configuration.Section>
+            )}
 
-            <Configuration.Section
-                title="Aspect Ratio"
-                description="Change the aspect ratio of your meme"
-            >
-                {config.memeUrl ? (
+            {config.memeUrl && (
+                <Configuration.Section
+                    title="Aspect Ratio"
+                    description="Change the aspect ratio of your meme"
+                >
                     <div className="flex flex-col gap-2">
                         <div className="flex flex-col gap-4">
                             <h3 className="text-base font-medium">Aspect Ratio</h3>
@@ -167,10 +161,8 @@ export default function Inspector() {
                             Reset Meme
                         </Button>
                     </div>
-                ) : (
-                    <h3 className="text-lg font-semibold">No meme generated</h3>
-                )}
-            </Configuration.Section>
+                </Configuration.Section>
+            )}
         </Configuration.Root>
     )
 }
