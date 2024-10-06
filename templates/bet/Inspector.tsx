596b759b27d60b320cd17560e552e065544d0488
kevinsslin <kk123750964@gmail.com>
2024-10-07 04:56:18 +0800

    fix: remove set button for reactivity

--- templates/bet/Inspector.tsx
+++ templates/bet/Inspector.tsx
@@ -1,90 +1,77 @@
 'use client'
-import { Button, Input, ColorPicker } from '@/sdk/components'
+import { Input, ColorPicker } from '@/sdk/components'
 import { useFrameConfig, useFarcasterId, useFarcasterName, useUploadImage } from '@/sdk/hooks'
 import { Configuration } from '@/sdk/inspector'
 import { Avatar, Switch } from '@/sdk/components'
-import { FrameError } from '@/sdk/error'
-
 import { getUserDataByFarcasterUsername } from './utils/farcaster'
-import { useRef, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import type { Config } from '.'
 
 export default function Inspector() {
     const [config, updateConfig] = useFrameConfig<Config>()
-
     const { privacy, claim, opponent, arbitrator, asset, amount, owner } = config
-
-    const claimInputRef = useRef<HTMLInputElement>(null)
-    const opponentInputRef = useRef<HTMLInputElement>(null)
-    const arbitratorInputRef = useRef<HTMLInputElement>(null)
-    const assetInputRef = useRef<HTMLInputElement>(null)
-    const amountInputRef = useRef<HTMLInputElement>(null)
 
     const fid = useFarcasterId()
     const username = useFarcasterName()
     const uploadImage = useUploadImage()
 
+    const [opponentInput, setOpponentInput] = useState('')
+    const [arbitratorInput, setArbitratorInput] = useState('')
+
     // Set owner if it's not already set
     useEffect(() => {
         const fetchData = async () => {
             if (username) {
-                try {
-                    const data = await getUserDataByFarcasterUsername(username);
-                    updateConfig({
-                        owner: {
-                            username,
-                            fid: data.fid,
-                            custody_address: data.custody_address,
-                            pfp_url: data.pfp_url,
-                        },
-                    });
-                } catch (err) {
-                    throw new FrameError(`Failed to fetch data for owner ${username}`);
-                }
+                const data = await getUserDataByFarcasterUsername(username)
+                updateConfig({
+                    owner: {
+                        username,
+                        fid: fid,
+                        custody_address: data.custody_address,
+                        pfp_url: data.pfp_url,
+                    },
+                })
             }
-        };
-        fetchData();
-    }, []);
+        }
+        fetchData()
+    }, [username, fid])
 
-    const handleSetRoles = async () => {
+    const handleSetRolesOnBlur = async (field: 'opponent' | 'arbitrator', value: string) => {
         try {
-            const opponentData = opponentInputRef.current?.value
-                ? await getUserDataByFarcasterUsername(opponentInputRef.current.value)
-                : null
-            const arbitratorData = arbitratorInputRef.current?.value
-                ? await getUserDataByFarcasterUsername(arbitratorInputRef.current.value)
-                : null
-
+            const data = await getUserDataByFarcasterUsername(value)
             updateConfig({
-                opponent: opponentData
-                    ? {
-                          username: opponentInputRef.current.value,
-                          fid: opponentData.fid,
-                          custody_address: opponentData.custody_address,
-                          pfp_url: opponentData.pfp_url,
-                      }
-                    : null,
-                arbitrator: arbitratorData
-                    ? {
-                          username: arbitratorInputRef.current.value,
-                          fid: arbitratorData.fid,
-                          custody_address: arbitratorData.custody_address,
-                          pfp_url: arbitratorData.pfp_url,
-                      }
-                    : null,
+                [field]: {
+                    username: value,
+                    fid: data.fid,
+                    custody_address: data.custody_address,
+                    pfp_url: data.pfp_url,
+                },
             })
 
-            if (opponentInputRef.current) opponentInputRef.current.value = ''
-            if (arbitratorInputRef.current) arbitratorInputRef.current.value = ''
+            // Reset input value once valid
+            if (field === 'opponent') {
+                setOpponentInput('')
+            } else {
+                setArbitratorInput('')
+            }
         } catch (error) {
-            throw new FrameError(`Invalid opponent or arbitrator farcaster username`)
+            updateConfig({
+                [field]: null, 
+            })
+
+            // Store the invalid input for display
+            if (field === 'opponent') {
+                setOpponentInput(value)
+            } else {
+                setArbitratorInput(value)
+            }
         }
     }
 
     return (
         <Configuration.Root>
-            {/* Gerneral and Cover Section */}
-            <Configuration.Section title="Gerneral and Cover">
+            {/* General and Cover Section */}
+            <Configuration.Section title="General and Cover">
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col gap-2">
                         <h2 className="text-lg text-white font-bold">Background Color</h2>
@@ -127,31 +114,24 @@ export default function Inspector() {
                             />
                             <p className="text-white font-semibold">Privacy Mode</p>
                         </div>
-                        <p className="text-gray-300">(if you open privacy mode, others cannot see your bet claim!)</p>
+                        <p className="text-gray-300">
+                            (Enabling privacy mode hides your bet claim from others!)
+                        </p>
                     </div>
                 </div>
             </Configuration.Section>
+
             {/* Claim Section */}
             <Configuration.Section title="Claim">
-                <p className="text-white">{JSON.stringify(config)}</p>
                 <p className="text-white">{claim || 'No claim set'}</p>
 
                 <div className="text-white flex flex-col gap-2">
                     <Input
                         className="text-lg text-white"
                         placeholder="I bet you that…"
-                        ref={claimInputRef}
+                        defaultValue={claim}
+                        onBlur={(e) => updateConfig({ claim: e.target.value })}
                     />
-                    <Button
-                        onClick={() => {
-                            if (!claimInputRef.current?.value) return
-                            updateConfig({ claim: claimInputRef.current.value })
-                            claimInputRef.current.value = ''
-                        }}
-                        className="w-full bg-border hover:bg-secondary-border text-white"
-                    >
-                        Set Claim
-                    </Button>
                 </div>
             </Configuration.Section>
 
@@ -159,48 +139,61 @@ export default function Inspector() {
             <Configuration.Section title="Roles">
                 <h2 className="text-white text-lg font-bold">Owner</h2>
                 <div className="flex flex-row gap-2 items-center">
-                    <Avatar.Root style={{ width: 36, height: 36 }}>
-                        <Avatar.Image src={owner?.pfp_url || defaultAvatar} />
-                    </Avatar.Root>
+                    {owner?.pfp_url && (
+                        <Avatar.Root style={{ width: 36, height: 36 }}>
+                            <Avatar.Image src={owner.pfp_url} />
+                        </Avatar.Root>
+                    )}
                     <p className="text-white">
-                        {owner?.username ? `${owner.username} (FID: ${owner.fid})` : 'No owner set'}
+                        {owner?.username
+                            ? `${owner.username} (FID: ${owner.fid})`
+                            : 'Please enter a valid owner username'}
                     </p>
                 </div>
+
                 <h2 className="text-white text-lg font-bold">Opponent</h2>
                 <div className="flex flex-row gap-2 items-center">
-                    <Avatar.Root style={{ width: 36, height: 36 }}>
-                        <Avatar.Image src={opponent?.pfp_url || defaultAvatar} />
-                    </Avatar.Root>
+                    {opponent?.pfp_url && (
+                        <Avatar.Root style={{ width: 36, height: 36 }}>
+                            <Avatar.Image src={opponent.pfp_url} />
+                        </Avatar.Root>
+                    )}
                     <p className="text-white">
-                        {opponent?.username ? `${opponent.username} (FID: ${opponent.fid})` : 'No opponent set'}
+                        {opponent?.username
+                            ? `${opponent.username} (FID: ${opponent.fid})`
+                            : opponentInput
+                            ? `${opponentInput} is invalid`
+                            : 'Please enter a valid opponent username'}
                     </p>
                 </div>
                 <Input
                     className="text-lg text-white"
                     placeholder="Opponent username"
-                    ref={opponentInputRef}
+                    defaultValue={opponentInput}
+                    onBlur={(e) => handleSetRolesOnBlur('opponent', e.target.value)}
                 />
+
                 <h2 className="text-white text-lg font-bold">Arbitrator</h2>
                 <div className="flex flex-row gap-2 items-center">
-                    <Avatar.Root style={{ width: 36, height: 36 }}>
-                        <Avatar.Image src={arbitrator?.pfp_url || defaultAvatar} />
-                    </Avatar.Root>
+                    {arbitrator?.pfp_url && (
+                        <Avatar.Root style={{ width: 36, height: 36 }}>
+                            <Avatar.Image src={arbitrator.pfp_url} />
+                        </Avatar.Root>
+                    )}
                     <p className="text-white">
-                        {arbitrator?.username ? `${arbitrator.username} (FID: ${arbitrator.fid})` : 'No arbitrator set'}
+                        {arbitrator?.username
+                            ? `${arbitrator.username} (FID: ${arbitrator.fid})`
+                            : arbitratorInput
+                            ? `${arbitratorInput} is invalid`
+                            : 'Please enter a valid arbitrator username'}
                     </p>
                 </div>
                 <Input
                     className="text-lg text-white"
                     placeholder="Arbitrator username"
-                    ref={arbitratorInputRef}
+                    defaultValue={arbitratorInput}
+                    onBlur={(e) => handleSetRolesOnBlur('arbitrator', e.target.value)}
                 />
-
-                <Button
-                    onClick={handleSetRoles}
-                    className="w-full bg-border hover:bg-secondary-border text-white"
-                >
-                    Set Roles
-                </Button>
             </Configuration.Section>
 
             {/* Prize Section */}
@@ -211,7 +204,8 @@ export default function Inspector() {
                 <Input
                     className="text-lg text-white"
                     placeholder="Asset address"
-                    ref={assetInputRef}
+                    defaultValue={asset}
+                    onBlur={(e) => updateConfig({ asset: e.target.value as Address })}
                 />
 
                 <h2 className="text-white text-xl font-bold">Amount</h2>
@@ -221,36 +215,9 @@ export default function Inspector() {
                     className="text-xl text-white"
                     placeholder="Amount"
                     type="number"
-                    ref={amountInputRef}
+                    defaultValue={amount || 0}
+                    onBlur={(e) => updateConfig({ amount: Number(e.target.value) })}
                 />
-
-                <Button
-                    onClick={() => {
-                        updateConfig({
-                            asset: assetInputRef.current?.value as Address || '0x0',
-                            amount: amountInputRef.current?.value ? Number(amountInputRef.current.value) : 0,
-                        })
-                        assetInputRef.current.value = ''
-                        amountInputRef.current.value = ''
-                    }}
-                    className="w-full bg-border hover:bg-secondary-border text-white"
-                >
-                    Set Prize
-                </Button>
-                {/* Reset Defaults */}
-                <Button
-                    variant="destructive"
-                    className="w-full text-white mt-8"
-                    onClick={() => updateConfig({
-                        claim: '',
-                        opponent: null,
-                        arbitrator: null,
-                        asset: '0x0',
-                        amount: 0,
-                    })}
-                >
-                    Reset Default
-                </Button>
             </Configuration.Section>
         </Configuration.Root>
     )
