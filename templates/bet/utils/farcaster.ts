432e155f7e7b504b6e42b5d0ad00d4b36d2ef492
kevinsslin <kk123750964@gmail.com>
2024-10-09 12:36:59 +0800

    refactor: improve error handling in getUserDataByFarcasterUsername function

--- templates/bet/utils/farcaster.ts
+++ templates/bet/utils/farcaster.ts
@@ -12,23 +12,24 @@ export async function getUserDataByFarcasterUsername(username: string) {
         },
     }
 
-    const r = (await fetch(`${NEYNAR_API_URL}/farcaster/user/search?q=${username}&limit=1`, options)
+    const r = (await fetch(`${NEYNAR_API_URL}/farcaster/user/search?q=${username}&limit=3`, options)
         .then((response) => response.json())
         .catch((err) => {
             throw new Error(`Invalid Farcaster username ${username}`)
         })) as { result: { users: any[] } }
 
-    const user = r?.result?.users?.[0];
-    if (!user) {
+    const users = r?.result?.users
+    if (!users || users.length === 0) {
         throw new Error(`User not found for username: ${username}`)
     }
 
-    const { fid, custody_address, username: returnedUsername, pfp_url } = user
+    for (const user of users) {
+        const { fid, custody_address, username: returnedUsername, pfp_url } = user
 
-    // check if this is the user we want
-    if (username != returnedUsername) {
-      throw new Error(`User not found for username: ${username}, returned ${returnedUsername}`)
+        // check if this is the user we want
+        if (username === returnedUsername) {
+            return { fid, custody_address, returnedUsername, pfp_url }
+        }
     }
-
-    return { fid, custody_address, returnedUsername, pfp_url }
+    throw new Error(`User not found for username: ${username}`)
 }
