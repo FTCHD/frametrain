17e22efb9a4690d12f298a66cdcb73c92940b6cf
Kevin Lin <86810837+kevinsslin@users.noreply.github.com>
2024-10-12 20:17:29 +0800

    fix: update validateConfig.ts according to config

--- templates/bet/utils/validateConfig.ts
+++ templates/bet/utils/validateConfig.ts
@@ -1,7 +1,7 @@
 import { FrameError } from '@/sdk/error'
 import type { Config } from '..'
 
-const requiredFields = ['claim', 'owner', 'opponent', 'arbitrator', 'asset', 'amount'] as const;
+const requiredFields = ['claim', 'owner', 'opponent', 'arbitrator', 'chain', 'token', 'amount'] as const;
 
 export function validateConfig(config: Config) {
   for (const field of requiredFields) {
