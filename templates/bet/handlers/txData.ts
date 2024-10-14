22e95af43a2aeed42b2315ca1c1dd5f8d35af869
kevinsslin <kk123750964@gmail.com>
2024-10-15 03:37:48 +0800

    fix: handle invalid data in txData handler

--- templates/bet/handlers/txData.ts
+++ templates/bet/handlers/txData.ts
@@ -16,32 +16,39 @@ export default async function txData({
         sessionId: string
     }
 }): Promise<BuildFrameData> {
-    if (!config.token || !config.amount) {
-        throw new FrameError('Token or amount not specified in the bet configuration.')
-    }
+    try {
+        if (!config.token || !config.amount) {
+            throw new FrameError('Token or amount not specified in the bet configuration.')
+        }
 
-    const glide = getGlide(config.chain)
+        const glide = getGlide(config.chain)
 
-    const session = await getSessionById(glide, params.sessionId)
+        const session = await getSessionById(glide, params.sessionId)
 
-    if (session.paymentStatus === 'paid') {
-        throw new FrameError('Payment already made')
-    }
+        if (session.paymentStatus === 'paid') {
+            throw new FrameError('Payment already made')
+        }
 
-    if (!session.unsignedTransaction) {
-        throw new FrameError('Missing transaction')
-    }
+        if (!session.unsignedTransaction) {
+            throw new FrameError('Missing transaction')
+        }
 
-    return {
-        transaction: {
-            chainId: session.unsignedTransaction.chainId,
-            method: 'eth_sendTransaction',
-            params: {
-                to: session.unsignedTransaction.to,
-                value: session.unsignedTransaction.value,
-                data: session.unsignedTransaction.input,
-                abi: [],
+        return {
+            transaction: {
+                chainId: session.unsignedTransaction.chainId,
+                method: 'eth_sendTransaction',
+                params: {
+                    to: session.unsignedTransaction.to,
+                    value: session.unsignedTransaction.value,
+                    data: session.unsignedTransaction.input
+                        ? `0x${session.unsignedTransaction.input.replace(/^0x/, '')}`
+                        : '0x', // to make sure the data is a valid hex string
+                    abi: [],
+                },
             },
-        },
+        }
+    } catch (e) {
+        const error = e as Error
+        throw new FrameError(error.message)
     }
 }
