f3f3d3f66bb353044949fd7ae5829cac5b7d44f3
FTCHD <far@fetched>
2024-10-29 20:53:47 +0200

    [compose] dynamic

--- app/api/compose/dynamic/page.tsx
+++ app/api/compose/dynamic/page.tsx
@@ -1,32 +1,27 @@
 
+'use client'
 
-export default function DynamicPage({
-    searchParams,
-}: {
-    searchParams: { q?: string }
-}) {
-    // Get the q parameter or use a random default URL
-    const defaultUrls = [
-        'https://neynar.com',
-        'https://x.com',
-        'https://twitter.com',
-        'https://google.com',
-    ]
-    const randomUrl = defaultUrls[Math.floor(Math.random() * defaultUrls.length)]
-    const url = searchParams.q || randomUrl
+import Script from 'next/script'
 
+export default function DynamicPage() {
     return (
-        <iframe
-            src={url}
-            style={{
-                width: '100vw',
-                height: '100vh',
-                border: 'none',
-                position: 'fixed',
-                top: 0,
-                left: 0,
-            }}
-            title="Dynamic Content"
-        />
+        // <embed
+        //     src={url}
+        //     style={{
+        //         width: '100vw',
+        //         height: '100vh',
+        //         border: 'none',
+        //         position: 'fixed',
+        //         top: 0,
+        //         left: 0,
+        //     }}
+        //     title="Dynamic Content"
+        // />
+        <div
+            className="calendly-inline-widget w-screen h-screen bg-red-500"
+            data-url="https://calendly.com/acmesales"
+        >
+            <Script src="https://assets.calendly.com/assets/external/widget.js" />
+        </div>
     )
 }
